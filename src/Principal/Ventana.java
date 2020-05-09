bc316abd2a33aa41f0dd2aa9e6b31d93d2d69d4b
ElianaOcampo <elianaocampo.contacto@gmail.com>
2020-05-09 20:30:08 -0300

    agregamos el ordenamiento por abecedario

--- src/Principal/Ventana.java
+++ src/Principal/Ventana.java
@@ -57,6 +57,7 @@ public class Ventana extends JFrame {
 		MenuItemListar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				contentPane.removeAll();
+				ordenarPeliculas();
 				ListarPeliculas panel = new ListarPeliculas();
 				panel.setDefaultListModel(listModel);
 				contentPane.add(panel);
@@ -71,6 +72,28 @@ public class Ventana extends JFrame {
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		
+	}
+	
+	public void ordenarPeliculas() {
+		Peliculas temp;
+	    Peliculas[] ob = new Peliculas[listModel.getSize()];
+	    for(int i = 0 ; i <listModel.getSize(); i++ )
+	        ob[i] = listModel.getElementAt(i);
+	    int n=ob.length;
+	    for(int i=0;i<n;i++)
+	        for(int j=0;j<n-i-1;j++)
+	        {
+	            if(ob[j].getNombre().compareTo(ob[j+1].getNombre())>0)
+	            {
+	             temp=ob[j];
+	             ob[j]=ob[j+1];
+	             ob[j+1]=temp;
+	            }
+
+	          }
+	    listModel.removeAllElements();
+	    for(int i=0;i<n;i++)
+	      listModel.addElement((Peliculas) ob[i]);
 	}
 
 }
